383ecb83fef0cd676eb4e97a9b028eab6811cd4e
mirzakabulovv <mirzakabulovv@gmail.com>
2024-12-19 18:10:17 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can add some simple interaction or validation for forms in the future
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("EduAI Website Loaded!");
+});
